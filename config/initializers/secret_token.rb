44eb8e9acf57ced3adea36c85acebab4847d20bb
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-29 21:23:04 +0200

    Start empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaolin::Application.config.secret_token = '70ee113cb7237bf2993450cbcf10ff0e85dd589c4209244bed555142e15ed7d14da8e66b6ca22413e218d018e527a27ba5ecf92858069f6ce6905d1a9e564760'
