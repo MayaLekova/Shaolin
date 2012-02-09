009fe9179740973ac8443b09e1bc292fafb2d3ef
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-02-09 22:05:42 +0200

    Move list logic from template to helpers for reuse

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,34 @@ module ApplicationHelper
     properties[:initial_value] ||= ""
     properties[:validators] ||= ""
     html = ""
-    html << "<div class='text-container%{hidden}%{mandatory}'>" % {
+    html << "<div class='text-container%{hidden}%{mandatory}'>\n" % {
                                                                     hidden: properties[:hidden] ? ' none' : '',
                                                                     mandatory: properties[:mandatory] ? ' mandatory-field' : '',
                                                                   }
-    html << "<label class='text-input-label' for='%{id}'>%{label}</label>"
-    html << "<input type='text' name='%{id}' id='%{id}' class='ui-corner-all text-input %{classes}' value='%{initial_value}' data-validators='%{validators}'/>"
-    html << "</div>"
+    html << "    <label class='text-input-label' for='%{id}'>%{label}</label>\n"
+    html << "    <input type='text' name='%{id}' id='%{id}' class='ui-corner-all text-input %{classes}' value='%{initial_value}' data-validators='%{validators}'/>\n"
+    html << "</div>\n"
 
     Rails.logger.debug html % properties
 
     html % properties
+  end
+
+  def list_resources(resources)
+    html = "<ul>\n"
+    resources.map do |resource|
+      html << "    <li>" 
+      html << "        <a href=\"#{resource.id}\"> #{resource.name} </a>"
+      html << "    </li>"
+    end
+    html << "</ul>"
+  end
+
+  def link_to_new
+    html = "   <a href=\"new\" class=\"ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only left add-new-button\">"
+    html << "       <span class=\"ui-icon ui-icon-plus left\"></span>"
+    html << "       <span class=\"right\">Add new</span>"
+    html << "   </a>"
+
   end
 end
