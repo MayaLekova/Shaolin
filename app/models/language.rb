61561c17499135f67733be159e17364f66d2c808
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-02-13 00:19:44 +0200

    Update Language and Lesson models

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,4 @@
 class Language < ActiveRecord::Base
   has_many :lessons
+  validates_presence_of :name
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,4 @@
 class Lesson < ActiveRecord::Base
   belongs_to :language
+  validates_presence_of :language
 end
