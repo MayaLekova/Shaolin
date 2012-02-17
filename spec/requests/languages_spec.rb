6fadc5d79e2da1ae2eff1162d72f7ad3812a82d7
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-02-17 22:07:57 +0200

    Update specs

--- spec/requests/languages_spec.rb
+++ spec/requests/languages_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "Languages" do
   describe "GET /languages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get languages_path
-      response.status.should be(200)
+    context "without a logged in administrator" do
+      it "fails" do
+        # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+        get lessons_path
+        response.status.should_not be(200)
+      end
+
+      it "redirects to the index" do
+        get lessons_path
+        response.should redirect_to root_url
+      end
     end
   end
 end
