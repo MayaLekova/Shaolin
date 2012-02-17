465525f948c437ca32042940436b0cc766aa15e0
Maya Lekova <apokalyptra@gmail.com>
2012-02-17 20:20:08 +0200

    Enhanced validation of language

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ActiveRecord::Base
   has_many :lessons
-  validates_presence_of :name
+  validates_presence_of :name, :command, :file_extension
+  validates_format_of :command, :with => /%{source_file}/
 end
