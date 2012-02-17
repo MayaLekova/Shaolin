50406077b92295a159af407146fadc686ed1c781
Maya Lekova <apokalyptra@gmail.com>
2012-02-17 12:34:21 +0200

    Second try for merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120216182354) do
+ActiveRecord::Schema.define(:version => 20120216214248) do
 
   create_table "languages", :force => true do |t|
     t.string   "name"
